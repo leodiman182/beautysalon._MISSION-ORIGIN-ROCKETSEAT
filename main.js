1f6ec0d68bb81cb531a604cb92925deba6fe7d6c
Leonardo Diman Martins <leonardo.diman@gmail.com>
2021-09-14 09:56:43 -0300

    First commit

---
+++ main.js
@@ -0,0 +1,33 @@
+/* TOGGLE MENU SCRIPT */
+
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/* ESCONDER MENU COM LINKS */
+
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/* ADICIONAR SHADOW NO HEADER AO DAR SCROLL */
+
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
